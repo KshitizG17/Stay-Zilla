2cc6617b5d26bcc0f3b95d71afec997a3addb505
KshitizG17 <kshitizrathour1401@gmail.com>
2025-05-11 11:03:40 +0530

    First commit of STAY-Zilla projects with website & database

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -20,7 +20,7 @@ define('USERS_FOLDER','users/');
 
 //sendgrid API key
 
-define('SENDGRID_API_KEY',"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");
+// define('SENDGRID_API_KEY',"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"); //Uncomment to use
 define('SENDGRID_EMAIL',"p.vishalkumar08@gmail.com");
 define('SENDGRID_NAME',"Group 10");
 
